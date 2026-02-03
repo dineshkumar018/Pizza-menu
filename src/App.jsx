048ab2f83cd4892633f2536f5961c0feb0dfc8ba
Dinesh Kumar <dineshkumar180603@gmail.com>
2026-02-03 19:33:23 +0530

    Initial commit - Pizza Menu React App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import Pizza from './pizza'
+
+const App = () => {
+  return (
+    <>
+    <h1>Hello React</h1>
+    <Pizza/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pizza.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Pizza = () => {
+  return (
+    <div>
+        <img src="Pizzas/spinaci.jpg" alt="spinaci pizza" />
+        <h2>Pizza Spinaci</h2>
+        <p>Tomato, monzerella, spinach and ricotto cheese</p>
+        <p>32</p>
+    </div>
+  )
+}
+
+export default Pizza
